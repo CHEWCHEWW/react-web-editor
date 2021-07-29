cda77dbce779a6b0c08064c00dc85b7f1610d69e
Minjieun <chewchewchew3@gmail.com>
2021-07-30 03:12:32 +0900

    [REFACTOR]: Separate useDraggable custom hook

--- src/hooks/useDraggable.ts
+++ src/hooks/useDraggable.ts
@@ -1,5 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
 
-const useDraggable = () => {};
+interface UseDraggableProps {
+  left: number
+  top: number
+}
+
+interface ComponentInfomation {
+  currentX: number
+  currentY: number
+  isDragging: boolean
+  location: UseDraggableProps
+}
+
+const useDraggable = ({ left, top }: UseDraggableProps) => {
+  const [componentInfomation, setComponentInformation] = useState<ComponentInfomation>({
+    currentX: left,
+    currentY: top,
+    isDragging: false,
+    location: {
+      left,
+      top,
+    }
+  });
+  
+  const handleDragStart = (ev: React.MouseEvent<HTMLDivElement>): void => {
+    const { clientX, clientY } = ev;
+    const { 
+      location: { 
+        left, 
+        top,
+      },
+    } = componentInfomation;
+
+    const currentX: number = clientX - left;
+    const currentY: number = clientY - top;
+    
+    setComponentInformation((prev) => ({
+      ...prev,
+      isDragging: true,
+      currentX,
+      currentY,
+    }));
+  };
+
+  const handleDragMove = (ev: React.MouseEvent<HTMLDivElement>): void => {
+    const { currentX, currentY, isDragging } = componentInfomation;
+
+    if (!isDragging || !currentX || !currentY) {
+      return;
+    }
+
+    const { clientX, clientY } = ev;
+
+    const left: number = clientX - currentX;
+    const top: number = clientY - currentY;
+    
+    setComponentInformation((prev) => ({
+      ...prev,
+      location: {
+        left,
+        top,
+      },
+    }));
+  };
+
+  const handleDragEnd = (): void => {
+    setComponentInformation((prev) => ({
+      ...prev,
+      isDragging: false,
+    }));
+  };
+
+  return {  
+    handleDragEnd, 
+    handleDragMove, 
+    handleDragStart,
+    componentLocation: componentInfomation.location,
+    isDragging: componentInfomation.isDragging,
+  };
+};
 
 export default useDraggable;
