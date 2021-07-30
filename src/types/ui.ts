f46ec69858cd5af7dbcc7221ab9b9140e497b472
Minjieun <chewchewchew3@gmail.com>
2021-07-30 11:37:25 +0900

    [ADD]: Resizable handler, but need to fix

--- src/types/ui.ts
+++ src/types/ui.ts
@@ -3,6 +3,11 @@ export interface ComponentLocation {
   top: number
 }
 
+export interface ComponentStyle extends ComponentLocation {
+  width: number
+  height: number
+}
+
 export interface DragListContent {
   number: string
   title: string
