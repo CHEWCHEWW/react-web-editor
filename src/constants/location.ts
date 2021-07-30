dd9c791919b28e5edbf91ca083e80318d15c7a82
Minjieun <chewchewchew3@gmail.com>
2021-07-30 11:20:27 +0900

    [ADD]: Resize Component basic ui

---
+++ src/constants/location.ts
@@ -0,0 +1,13 @@
+const TOP_LEFT = "top-left";
+const TOP_RIGHT = "top-right";
+const BOTTOM_LEFT = "bottom-left";
+const BOTTOM_RIGHT = "bottom-right";
+
+export const DIRECTION = {
+  TOP_LEFT,
+  TOP_RIGHT,
+  BOTTOM_LEFT,
+  BOTTOM_RIGHT,
+};
+
+export const DIRECTIIONS = [TOP_LEFT, TOP_RIGHT, BOTTOM_LEFT, BOTTOM_RIGHT];
