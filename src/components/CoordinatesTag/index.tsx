4d91ba37af6da9607ba0ed743032cf8f29329556
Minjieun <chewchewchew3@gmail.com>
2021-08-01 18:33:39 +0900

    [ADD]: Coordinates tag to editor

---
+++ src/components/CoordinatesTag/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import styled from "styled-components";
+
+interface CoordinatesTagProps {
+  left: number
+  top: number
+}
+
+const CoordinatesTag = ({ left, top }: CoordinatesTagProps): React.ReactElement => {
+  return (
+    <Tag>
+      {`x: ${left} y: ${top}`}
+    </Tag>
+  );
+};
+
+const Tag = styled.div`
+  width: 90px;
+  height: 20px;
+  top: -30px;
+  left: -30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  background-color: ${({ theme }) => theme.gray};
+  color: white;
+  font-size: 0.8rem;
+  font-weight: bold;
+  border-radius: 5px;
+  opacity: 0.8;
+`;
+
+export default CoordinatesTag;
