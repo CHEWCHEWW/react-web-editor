6ed34f33b8bd13f9d5e75be5f8d04fa975bd71bb
Minjieun <chewchewchew3@gmail.com>
2021-08-05 21:40:40 +0900

    [ADD]: Text editor component

--- src/components/TextInput/index.tsx
+++ src/components/TextInput/index.tsx
@@ -1,27 +1,52 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 import { BiText } from "react-icons/bi";
 
-import CustomInput from "../CustomInput";
+import Icon from "../Icon";
 
-const TextInput = (): React.ReactElement => {
+const TextInput: React.FC = (): React.ReactElement => {
   const [text, setText] = useState<string>("");
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+
+  const handleIconClick = () => {
+    setIsEditing(prev => !prev);
+  };
 
   const handleTextChange = (ev: React.ChangeEvent<HTMLInputElement>) => {
     setText(ev.target.value);
-    console.log(text);
   };
 
   return (
-    <CustomInput
-      type="text"
-      onChange={handleTextChange}
-      top={44}
-      value={text}
-      right={-21}
-    >
-      <BiText />
-    </CustomInput>
+    <>
+      <Icon
+        top={0}
+        right={-21}
+        onClick={handleIconClick}
+      >
+        <BiText />
+      </Icon>
+      {isEditing && <Input onChange={handleTextChange} value={text} autoFocus/>}
+      <TextArea>{text}</TextArea>
+    </>
   );
 };
+
+const Input = styled.input`
+  width: 90%;
+  height: 90%;
+  position: absolute;
+  font-size: 100px;
+  opacity: 0;
+  z-index: 8;
+`;
+
+const TextArea = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  font-size: 100px;
+  color: pink;
+  z-index: 8;
+`;
 
 export default TextInput;
