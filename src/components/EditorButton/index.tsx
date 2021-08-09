af313082ed22ab04537ad8ba1acde20de6793aca
Minjieun <chewchewchew3@gmail.com>
2021-08-09 13:06:16 +0900

    [ADD]: Text execcommand button

---
+++ src/components/EditorButton/index.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import styled from "styled-components";
+
+interface EditorButtonProps {
+  name: string
+  isShowing: boolean
+  value: string
+}
+
+const EditorButton: React.FC<EditorButtonProps> = ({
+  name,
+  isShowing,
+  value,
+  children,
+}): React.ReactElement => {
+  const handleTextStyleChange = (ev: React.MouseEvent<HTMLDivElement>) => {
+    ev.preventDefault();
+    
+    document.execCommand(name, isShowing, value);
+  };
+
+  return (
+    <Button onMouseDown={handleTextStyleChange}>
+      {children}
+    </Button>
+  );
+};
+
+const Button = styled.div`
+  display: flex;
+  width: 1.5rem;
+  height: 1.5rem;
+  padding: 0.2rem 0.1rem;
+  color: #6c9eeb;
+  align-items: center;
+  justify-content: center;
+
+  :hover {
+    background-color: #f3f0f0;
+  }
+`;
+
+export default EditorButton;
