fa1fb09364df5c9b94bfd171093b9dfc5b18e5d2
Minjieun <chewchewchew3@gmail.com>
2021-08-12 12:58:22 +0900

    [FIX]: Color picker bug

--- src/components/ColorPicker/index.tsx
+++ src/components/ColorPicker/index.tsx
@@ -4,23 +4,23 @@ import { IoIosColorPalette } from "react-icons/io";
 import CustomInput from "../CustomInput";
 import { TypedCustomInputProps } from "../../types/handler";
 
-const ColorPicker: React.FC<TypedCustomInputProps> = ({ 
-  onChange, 
-  left, 
-  top, 
-  bottom, 
+const ColorPicker: React.FC<TypedCustomInputProps> = ({
+  onChange,
+  left,
+  top,
+  bottom,
   right,
   value,
 }): React.ReactElement => {
   return (
-    <CustomInput 
-      type="color" 
+    <CustomInput
+      type="color"
       onChange={onChange}
       top={top}
       right={right}
       left={left}
       bottom={bottom}
-      value={value}
+      value={value !== "#00ff0000" ? value : "#ffffff"}
     >
       <IoIosColorPalette />
     </CustomInput>
