8ad7f5d6511da17877707ce825aecdc5a55d11e3
Minjieun <chewchewchew3@gmail.com>
2021-08-19 13:58:13 +0900

    [FIX]: Change Import components

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
-export { default as useDragAndDrop } from "./hooks/useDragAndDrop";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+);
+
 export { default as useDraggable } from "./hooks/useDraggable";
 export { default as useImage } from "./hooks/useImage";
-export { default as useMouseEvent } from "./hooks/useMouseEvent";
 export { default as useResize } from "./hooks/useResize";
-export { default as useText } from "./hooks/useText";
+export { default as useColor } from "./hooks/useColor";
 
 export { default as EditableBoard } from "./components/EditableBoard";
-export { default as EditorBlock } from "./components/EditorBlock";
 export { default as DragAndDropTable } from "./components/DragAndDropTable";
 export { default as StyleEditorBlock } from "./components/StyleEditorBlock";
 export { default as TextEditorBlock } from "./components/TextEditorBlock";
