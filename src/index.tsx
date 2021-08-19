d36e71cb2998fb382d8f5ef5e680ead202837284
Minjieun <chewchewchew3@gmail.com>
2021-08-19 18:13:19 +0900

    [ADD]: README draft

--- src/index.tsx
+++ src/index.tsx
@@ -11,11 +11,10 @@ export { default as useImage } from "./hooks/useImage";
 export { default as useResize } from "./hooks/useResize";
 export { default as useColor } from "./hooks/useColor";
 
-export { default as EditableBoard } from "./components/EditableBoard";
 export { default as DragAndDropTable } from "./components/DragAndDropTable";
 export { default as StyleEditorBlock } from "./components/StyleEditorBlock";
 export { default as TextEditorBlock } from "./components/TextEditorBlock";
 
+export { default as EditableBoard } from "./components/EditableBoard";
 export { default as EditorBlockWrapper } from "./components/shared/EditorBlockWrapper";
 export { default as ResizeHandlerWrapper } from "./components/shared/ResizeHandlerWrapper";
-export { default as DraggableHandler } from "./components/shared/DraggableHandler";
